0f15c0a6f4a32ac8e53f47f2230ed1fe7ad16ff2
Bovard Tiberi <bovard.tiberi@gmail.com>
2013-01-17 19:15:42 -0700

    Added Artillery Shoot method.

---
+++ behavior/lib/ArtilleryShoot.java
@@ -0,0 +1,29 @@
+package team122.behavior.lib;
+
+import team122.robot.Artillery;
+import battlecode.common.GameActionException;
+import battlecode.common.GameObject;
+import battlecode.common.RobotType;
+
+public class ArtilleryShoot extends Behavior{
+	
+	protected Artillery robot;
+
+	@Override
+	public void run() throws GameActionException {
+		if (robot.rc.isActive()) {
+			GameObject[] robots = robot.rc.senseNearbyGameObjects(GameObject.class, RobotType.ARTILLERY.attackRadiusMaxSquared, robot.info.enemyTeam);
+			
+			if (robots.length > 0) {
+				robot.rc.attackSquare(robot.rc.senseLocationOf(robots[0]));
+			}
+		} // end is active.
+		
+	}
+
+	@Override
+	public boolean pre() {
+		return robot.canShoot && robot.enemyNearby;
+	}
+
+}
